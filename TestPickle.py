cc41fb2092f093e7d96f8554d2222ae2c3966373
Mr BeetMaker <pwollsen@gmail.com>
2024-05-29 15:46:46 +0200

    dont worry about it ok?

--- TestPickle.py
+++ TestPickle.py
@@ -3,32 +3,29 @@ import pickle
 
 pickle_dump_file = ".\\dump_file.bin"
 
+class Adder():
+
+    def __init__(self) -> None:
+        self._val = 10
+
+    def add(a, b):
+        1 + 2
+        a + b
+        2 + 1
+        b + a
+        return a + b + b + a + b + a
+
 class TestPickle(unittest.TestCase):
 
     def test(self):
-
-        class Adder():
-
-            def __init__(self) -> None:
-                self._val = 10
-
-            def add(a, b):
-                1 + 2
-                a + b
-                2 + 1
-                b + a
-                return a + b + b + a + b + a
-
         before = Adder()
-
         with open(pickle_dump_file, 'wb') as f:
             pickle.dump(before, f)
 
         with open(pickle_dump_file, 'rb') as f:
             after = pickle.load(f)
 
-
-        self.assertEqual(before, after)
+        self.assertEqual(hash(before), hash(after))
 
 if __name__ == '__main__':
     unittest.main()
