3dfc90ef7b1816b6fa903d228a466c5a24ab7070
Mr BeetMaker <pwollsen@gmail.com>
2024-05-30 15:21:56 +0200

    Added some shit

--- TestPickle.py
+++ TestPickle.py
@@ -16,8 +16,6 @@ class RecursiveClass:
     def __init__(self):
         self.self = self
 
-
-
 class TestPickle(unittest.TestCase):
 
     def __init__(self, methodName: str = "runTest") -> None:
@@ -28,11 +26,13 @@ class TestPickle(unittest.TestCase):
 
     @staticmethod
     def pickle_and_hash(data):
+        """Returns the hash of the pickled data."""
         pickled_data = pickle.dumps(data, protocol=4)
         return hashlib.sha256(pickled_data, usedforsecurity= False).hexdigest()
 
     @staticmethod
     def pickle_unpickle_repickle_and_hash(data):
+        """Pickles the data, unpickles it, pickles it again and hashes it."""
         pickled_data = pickle.dumps(data, protocol=4)
         unpickled_data = pickle.loads(pickled_data)
         re_pickled_data = pickle.dumps(unpickled_data, protocol=4)
@@ -40,14 +40,22 @@ class TestPickle(unittest.TestCase):
 
     @staticmethod
     def pickle_and_unpickle(data):
+        """Returns data after it has been pickled and unpickled."""
         pickled_data = pickle.dumps(data, protocol=4)
         unpickled_data = pickle.loads(pickled_data)
         return unpickled_data
 
+    def assertUnpickleEqual(self, data):
+        """Asserts that the data is the same before and after being unpickled."""
+        
+        unpickled_data = self.pickle_and_unpickle(data)        
+        self.assertEquals(data, unpickled_data)
+
     def assertPickleHashIdentical(self, data):
+        """Asserts that the hashes of the data is the same after being pickled once, unpickled and then pickled again.
+        Appends the results to the results file."""
         pickled_data = self.pickle_and_hash(data)
-        unpickled_data = self.pickle_and_unpickle(data)
-        repickled_data = self.pickle_and_hash(unpickled_data)
+        repickled_data = self.pickle_unpickle_repickle_and_hash(data)
 
         # Save pickled data
         with open(self.file_name, "a") as f:
@@ -57,6 +65,9 @@ class TestPickle(unittest.TestCase):
 
     def test_int(self):
         self.assertPickleHashIdentical(42)
+
+    def test_function(self):
+        self.assertPickleHashIdentical(pow)
 
     def test_float(self):
         self.assertPickleHashIdentical(3.1415926535)
@@ -80,7 +91,8 @@ class TestPickle(unittest.TestCase):
         self.assertPickleHashIdentical(SimpleClass(10))
 
     def test_recursive_class(self):
-        self.assertPickleHashIdentical(RecursiveClass(10))
+        self.assertPickleHashIdentical(RecursiveClass())
 
 if __name__ == '__main__':
+
     unittest.main()
