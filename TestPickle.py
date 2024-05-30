4b8b89b99bf0f2d528c080d0493833b0d23e78be
Mr BeetMaker <pwollsen@gmail.com>
2024-05-30 14:52:44 +0200

    its k

--- TestPickle.py
+++ TestPickle.py
@@ -1,8 +1,9 @@
 import unittest
 import pickle
 import hashlib
+import platform
+import sys
 
-pickle_dump_file = ".\\dump_file.bin"
 
 class SimpleClass:
     def __init__(self, value):
@@ -15,21 +16,43 @@ class RecursiveClass:
     def __init__(self):
         self.self = self
 
-def pickle_and_hash(data):
-    pickled_data = pickle.dumps(data, protocol=4)
-    return hashlib.sha256(pickled_data, usedforsecurity= False).hexdigest()
 
-def pickle_unpickle_repickle_and_hash(data):
-    pickled_data = pickle.dumps(data, protocol=4)
-    unpickled_data = pickle.loads(pickled_data)
-    re_pickled_data = pickle.dumps(unpickled_data, protocol=4)
-    return hashlib.sha256(re_pickled_data, usedforsecurity= False).hexdigest()
 
 class TestPickle(unittest.TestCase):
 
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        os_name = platform.system()
+        python_version = sys.version.split()[0]
+        self.file_name = f"results_{os_name}_{python_version}.csv"
+
+    @staticmethod
+    def pickle_and_hash(data):
+        pickled_data = pickle.dumps(data, protocol=4)
+        return hashlib.sha256(pickled_data, usedforsecurity= False).hexdigest()
+
+    @staticmethod
+    def pickle_unpickle_repickle_and_hash(data):
+        pickled_data = pickle.dumps(data, protocol=4)
+        unpickled_data = pickle.loads(pickled_data)
+        re_pickled_data = pickle.dumps(unpickled_data, protocol=4)
+        return hashlib.sha256(re_pickled_data, usedforsecurity= False).hexdigest()
+
+    @staticmethod
+    def pickle_and_unpickle(data):
+        pickled_data = pickle.dumps(data, protocol=4)
+        unpickled_data = pickle.loads(pickled_data)
+        return unpickled_data
+
     def assertPickleHashIdentical(self, data):
-        pickled_data = pickle_and_hash(data)
-        repickled_data = pickle_unpickle_repickle_and_hash(data)
+        pickled_data = self.pickle_and_hash(data)
+        unpickled_data = self.pickle_and_unpickle(data)
+        repickled_data = self.pickle_and_hash(unpickled_data)
+
+        # Save pickled data
+        with open(self.file_name, "a") as f:
+            f.write(f"{pickled_data},{repickled_data}\n")
+
         self.assertEqual(pickled_data, repickled_data)
 
     def test_int(self):
@@ -55,6 +78,9 @@ class TestPickle(unittest.TestCase):
 
     def test_simple_class(self):
         self.assertPickleHashIdentical(SimpleClass(10))
+
+    def test_recursive_class(self):
+        self.assertPickleHashIdentical(RecursiveClass(10))
 
 if __name__ == '__main__':
     unittest.main()
